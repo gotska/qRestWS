4e4a26cb549e4e7e53b45386b618dab98e7ea31c
Vladislav <gozkavlad@gmail.com>
2013-12-26 10:43:07 -0600

    added support many parameter in html get request

--- app/controllers/SqlQueryContr.scala
+++ app/controllers/SqlQueryContr.scala
@@ -23,17 +23,11 @@ object SqlQueryContr extends Controller {
     )(SqlQuery.apply)(SqlQuery.unapply)
   )
   
-  /*def editCategory(catId: Long) = { implicit request =>
-    val mapCat = Category.findAll.map(t =>t.id.toString -> t.name)
-    Category.findById(catId).map { cat =>
-      Ok(views.html.admin.category(catId,categoryForm.fill(cat), mapCat))
-    }.getOrElse(NotFound) 
-  }*/ 
- 
   def newQuery = Action { implicit request =>
     Ok(views.html.query("", queryForm))
      
   }
+ 
   /**
    * Handle the 'order form' submission.
    */
@@ -47,12 +41,6 @@ object SqlQueryContr extends Controller {
       }
     )    
   }
-  
-  /*def delCategory(catId: String) = Action { implicit request =>
-    SqlQuery.delete(catId)
-      Ok(views.html.admin.categories(Category.findAll()))
-     
-  }*/
   
 }
 
--- app/util/Parser.scala
+++ app/util/Parser.scala
@@ -4,23 +4,51 @@ import models._
 
 object Parser {
 
+   // prepare sql query for select
   def getSqlQuery(name: String): String = {
     if (name.contains('(')) {
-      val str = SqlQuery.findByNameWithParam(parseName(name))
-      addParam(str, parseParam(name))
+      val queryName = parseName(name)
+      val str = SqlQuery.findByNameWithParam(queryName)
+      createSqlQueryWithParam(str, name)
     } else
       SqlQuery.findByName(name)
   }
   
+   // get name 
   def parseName(name: String):String = {
     name.subSequence(0, name.indexOf('(')).toString()
    }
   
-  def parseParam(name: String):String = {
-    name.subSequence(name.indexOf('(')+1, name.indexOf(')')).toString()
-  }
+  // create sql query from string
+  def createSqlQueryWithParam(str: String,fn: String):String = {
+   val strWithOutBrackets = getStringParamBetweenBrackets(fn)
+   val strSeq = stringToStringList(strWithOutBrackets).seq
   
-  def addParam(str: String,param:String):String = {
-    str.subSequence(0, str.indexOf('{'))+param
-  }
+   addParam(str,strSeq)
+   
+  }          
+  
+  def getStringParamBetweenBrackets(str: String):String = {
+    str.subSequence(str.indexOf('(')+1, str.indexOf(')')).toString()
+  }                                              
+  
+  def stringToStringList (str: String): Seq[String] ={
+	str.split(',')
+  }                                              
+  
+  
+  def addParam(str: String, param:Seq[String]): String = {
+    if(!param.tail.isEmpty){
+     addParam(paramReplace(str,param.head),param.tail)
+    }
+    else{
+	   paramReplace(str,param.head)
+    }
+  }                                              
+ 
+  def paramReplace (str:String, prm: String): String ={
+		val str1 = str.subSequence(str.indexOf('{'), str.indexOf('}')+1)
+		str.replace(str1, prm)
+	}                                        
+	
 }
