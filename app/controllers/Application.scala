e14cebbb6bfea47e52b7d76f72fb33181c7e0854
gotska <gozkavlad@gmail.com>
2014-03-23 23:57:19 +0200

    add POST

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -14,6 +14,10 @@ object Application extends Controller {
     Ok(views.html.index(Option(SqlQuery.findAll)))
   }
 
+  def test = Action {
+    Ok(views.html.test())
+  }
+
    def get(name:String) = Action { request =>
     val map = request.queryString.map { case (k,v) => k -> v.mkString }
     val query = Parser.getSqlQuery(map)
@@ -22,5 +26,13 @@ object Application extends Controller {
    
     Ok(json)
   }
-    
+
+  def post = Action { request =>
+    val map = request.body.asFormUrlEncoded.getOrElse(Map()).map { case (k:String, v:List[String]) => k -> v.mkString }
+    val query = Parser.getSqlQuery(map)
+    val result = Table.sqlQueryWithName(query)
+    val json = Json.toJson(result)
+
+    Ok(json)
+  }
 }
